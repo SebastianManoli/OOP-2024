876b7ee73d3fc712628cc4b6046a5afd9e26ee96
SebastianManoli <C23375341@mytudublin.ie>
2024-01-24 18:06:46 +0000

    Completed Lab

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -2,34 +2,36 @@
 // Packages must match the folder structure
 package ie.tudublin;
 
-public class Main
-{
-	public void doDogStuff()
-	{
+public class Main {
+	public void doDogStuff() {
 		// Create a new Dog called Misty
 		// Call the constructor
 		Dog misty = new Dog("Misty");
 		misty.speak();
 
-        Dog lucy = new Dog("Lucy");
+		Dog lucy = new Dog("Lucy");
 		lucy.speak();
 	}
 
-	public void doCatStuff()
-	{
+	public void doCatStuff() {
 		// Put your code here
 		System.out.println("cats are from space");
+
+		Cat ginger = new Cat("Ginger");
+		while (ginger.getNumLives() > 0) {
+			ginger.kill();
+		}
+
 	}
-	
-	public static void main(String[] args)
-	{
+
+	public static void main(String[] args) {
 		System.out.println("Hello world");
-		
+
 		Main m = new Main();
 
 		m.doDogStuff();
 
 		m.doCatStuff();
 	}
-	
+
 }
